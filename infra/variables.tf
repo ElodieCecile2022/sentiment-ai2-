83f2562af4eead52ea787af6d3fe0a183489dbd6
Elodie Iparraguirre <elodie.iparraguirre83@gmail.com>
2026-06-25 22:36:14 +0200

    Mise à jour finale du Jenkinsfile et de l'infra

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+# infra/variables.tf
+
+variable "image_tag" {
+  description = "Tag de l'image Docker a deployer"
+  type        = string
+  default     = "latest"
+}
+
+variable "app_port" {
+  description = "Port expose en staging"
+  type        = number
+  default     = 8001
+}
+
+variable "container_name" {
+  description = "Nom du conteneur staging"
+  type        = string
+  default     = "sentiment-staging"
+}
